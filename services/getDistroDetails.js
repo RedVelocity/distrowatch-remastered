86328292c56d916ec3004a7f45992750c631d235
Shah Nawaz Khan <blooshaanster@gmail.com>
2022-01-19 18:28:37 +0530

    replace save() with create() getdistrodetails

--- services/getDistroDetails.js
+++ services/getDistroDetails.js
@@ -12,15 +12,12 @@ const getDistroDetails = async (slug) => {
     if (!distro) {
       const { data } = await axios.get(API_ENDPOINT, {
         headers: {
-          'User-Agent':
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',
+          'User-Agent': 'Chrome/96.0.4664.93',
         },
       });
       // Call helper function to scrape data
-      const newDistro = new Distro(buildDistroDetails(data, slug));
       // Save scraped data to DB and return it
-      await newDistro.save();
-      // console.log(`newDistro.toObject()`, newDistro.toObject());
+      const newDistro = await Distro.create(buildDistroDetails(data, slug));
       return newDistro.toObject();
     }
     // Return cached data from DB
