53c4a90c6eae68ede1769336839b65664655135a
Shah Nawaz Khan <blooshaanster@gmail.com>
2022-01-19 18:48:36 +0530

    implement findOneAndUpdate with upsert  getdistrodetails

--- services/getDistroDetails.js
+++ services/getDistroDetails.js
@@ -5,6 +5,8 @@ import Distro from '../models/Distro';
 
 const getDistroDetails = async (slug) => {
   const API_ENDPOINT = `https://distrowatch.com/table.php?distribution=${slug}`;
+  const USER_AGENT =
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36';
   try {
     await dbConnect();
     const distro = await Distro.findOne({ slug }).lean();
@@ -12,13 +14,18 @@ const getDistroDetails = async (slug) => {
     if (!distro) {
       const { data } = await axios.get(API_ENDPOINT, {
         headers: {
-          'User-Agent': 'Chrome/96.0.4664.93',
+          'User-Agent': USER_AGENT,
         },
       });
       // Call helper function to scrape data
       // Save scraped data to DB and return it
-      const newDistro = await Distro.create(buildDistroDetails(data, slug));
-      return newDistro.toObject();
+      const newDistro = await Distro.findOneAndUpdate(
+        { slug },
+        buildDistroDetails(data, slug),
+        { upsert: true, new: true }
+      );
+      // console.log(`newDistro`, newDistro);
+      return newDistro;
     }
     // Return cached data from DB
     return distro;
