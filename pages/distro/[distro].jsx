7ff6bc74f19b1f242f7bfc75741028271cdbcfd2
Shah Nawaz Khan <blooshaanster@gmail.com>
2022-01-21 15:28:40 +0530

    containerize app

--- pages/distro/[distro].jsx
+++ pages/distro/[distro].jsx
@@ -50,13 +50,14 @@ const DistroDetails = ({ pageData }) => {
       <Head>
         <title>{`Distrowatch | ${distro.header.title}`}</title>
       </Head>
-      <section className=" bg-primary h-[18rem] flex items-center justify-center">
-        <div className="grid items-center justify-center text-center justify-items-center holder md:flex">
-          <div className="relative w-40 h-40 mx-4 overflow-hidden bg-white border-2 rounded-full">
-            <Image {...logo} />
-          </div>
-          <h1>{distro.header?.title}</h1>
-          {/* <div className="flex items-center my-4 justify-evenly md:flex-row">
+      <div className="page-container">
+        <section className=" bg-primary h-[18rem] flex items-center justify-center">
+          <div className="grid items-center justify-center gap-2 text-center justify-items-center holder md:flex">
+            <div className="relative w-40 h-40 mx-4 overflow-hidden bg-white border-2 rounded-full">
+              <Image {...logo} />
+            </div>
+            <h1>{distro.header?.title}</h1>
+            {/* <div className="flex items-center my-4 justify-evenly md:flex-row">
             <ul className="p-2 m-2 font-semibold text-center md:text-left md:w-1/2">
               {
                 // attribute[0]=column heading, attribute[1]=[column values]
@@ -80,35 +81,32 @@ const DistroDetails = ({ pageData }) => {
             </ul>
           </div>
           <p className="">{distro.header.description}</p> */}
-        </div>
-      </section>
-      {banner && (
-        <section className="relative h-full md:h-[18rem] aspect-video holder -my-6 mx-8 md:mx-auto overflow-hidden rounded-2xl shadow-xl bg-gray-400">
-          <Image src={distro.header.banner} layout="fill" priority />
-        </section>
-      )}
-      <section
-        className={`bg-secondary texture flex items-center justify-center ${
-          banner && 'mt-16'
-        }`}
-      >
-        <p className="text-gray-200/90 holder">{distro.header.description}</p>
-      </section>
-      <section className=" bg-accent h-[18rem]">
-        <div className="holder">
-          <div className="inline-block w-auto px-4 py-2 bg-white rounded">
-            <h5>Rating</h5>
-            <span
-              className={`p-2 text-4xl ${
-                distro.rating > 8 ? 'text-accent' : 'text-red-400'
-              }`}
-            >
-              {distro.rating}
-            </span>
-            / 10
           </div>
-        </div>
-      </section>
+        </section>
+        {banner && (
+          <section className="relative h-full md:h-[18rem] aspect-video holder -my-6 mx-8 md:mx-auto overflow-hidden rounded-2xl shadow-xl bg-gray-400">
+            <Image src={distro.header.banner} layout="fill" priority />
+          </section>
+        )}
+        <section className={`bg-accent ${banner && 'mt-16'}`}>
+          <div className="holder">
+            <div className="card">
+              <h5>Rating</h5>
+              <span
+                className={`text-4xl ${
+                  distro.rating > 8 ? 'text-accent' : 'text-red-400'
+                }`}
+              >
+                {distro.rating}
+              </span>
+              <span className="text-gray-400"> / 10</span>
+            </div>
+          </div>
+        </section>
+        <section className="flex items-center justify-center bg-secondary texture">
+          <p className="text-gray-200/90 holder">{distro.header.description}</p>
+        </section>
+      </div>
     </>
   );
 };
