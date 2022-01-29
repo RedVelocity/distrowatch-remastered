defc2072d10855ee53451d947920f49527759fb2
Shah Nawaz Khan <blooshaanster@gmail.com>
2022-01-29 20:09:54 +0530

    add flags fix global

--- services/getCountryFlags.js
+++ services/getCountryFlags.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const getCountryFlags = async (countryName) => {
+  const country = countryName === 'Global' ? 'EU' : countryName;
   const { data } = await axios.get(
-    `https://restcountries.com/v3.1/name/${countryName}`
+    `https://restcountries.com/v3.1/name/${country}`
   );
   return data[0].flags.svg;
 };
