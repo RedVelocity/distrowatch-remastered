f0b140af92fb63b52ef112df6a200f8912eebb86
Shah Nawaz Khan <blooshaanster@gmail.com>
2022-01-31 13:50:34 +0530

    created Card sub components

--- components/AttributesSection/index.jsx
+++ components/AttributesSection/index.jsx
@@ -1,32 +1,76 @@
+import Image from 'next/image';
 import Card from '../Card';
-import getRatingProps from './getRatingProps';
-import getPopularityProps from './getPopularityProps';
+import getTextColor from './getTextColor';
 
 const AttributesSection = ({ marginRequired, rating, attributes }) => {
-  const ratingProps = getRatingProps(rating);
-  const popularityProps = getPopularityProps(attributes.popularity);
+  const ratingTextColor = getTextColor('rating', rating);
+  const popularityTextColor = getTextColor('popularity', attributes.popularity);
   return (
     <section className={`bg-accent ${marginRequired && 'mt-16'}`}>
       <div className="holder responsive-grid gap-4">
-        <Card {...ratingProps} />
-        <Card {...popularityProps} />
-        <Card
-          title="Origin"
-          icon="fa-globe-africa"
-          variant="flags"
-          flags={attributes.flags}
-        />
-        <Card
-          title="Status"
-          variant="singleHeading"
-          main={attributes.status.split(' ')[0]}
-          textColor={
-            attributes.status.includes('defined')
-              ? 'text-danger'
-              : 'text-success'
-          }
-          icon="fa-check-circle"
-        />
+        <Card title="User Rating" icon="fa-chart-bar">
+          <Card.Content>
+            {rating > 0 ? (
+              <Card.MainContent
+                text={rating.toFixed(2)}
+                textColor={ratingTextColor}
+                large
+              >
+                <Card.SubContent text=" / 10" />
+              </Card.MainContent>
+            ) : (
+              <Card.MainContent text="Not Available" />
+            )}
+          </Card.Content>
+        </Card>
+        <Card title="Popularity" icon="fa-chart-line">
+          <Card.Content>
+            {attributes.popularity.length > 0 ? (
+              <Card.MainContent
+                text={attributes.popularity[0]}
+                textColor={popularityTextColor}
+                large
+              >
+                <Card.SubContent text={` (${attributes.popularity[1]})`} />
+              </Card.MainContent>
+            ) : (
+              <Card.MainContent
+                text="Not Available"
+                textColor={popularityTextColor}
+              />
+            )}
+          </Card.Content>
+        </Card>
+        <Card title="Origin" icon="fa-globe-africa">
+          <Card.Content>
+            {attributes.flags.map((flag, index) => (
+              <div
+                className="relative aspect-[5/3] w-14 overflow-hidden rounded"
+                key={index}
+              >
+                <Image
+                  src={flag.flag}
+                  alt={flag.country}
+                  title={flag.country}
+                  layout="fill"
+                  priority
+                />
+              </div>
+            ))}
+          </Card.Content>
+        </Card>
+        <Card title="Status" icon="fa-check-circle">
+          <Card.Content>
+            <Card.MainContent
+              text={attributes.status.split(' ')[0]}
+              textColor={
+                attributes.status.includes('defined')
+                  ? 'text-danger'
+                  : 'text-success'
+              }
+            />
+          </Card.Content>
+        </Card>
       </div>
     </section>
   );
--- components/Card.jsx
+++ components/Card.jsx
@@ -1,65 +1,36 @@
-import Image from 'next/image';
+const Card = ({ title, icon, children }) => (
+  <div className="card flex flex-col">
+    <h5 className="border-b mb-2 pb-2">
+      {icon && <i className={`fas ${icon}`} />}
+      {` ${title}`}
+    </h5>
+    {children}
+  </div>
+);
 
-const Card = ({
-  title,
-  main,
-  sub,
-  textColor,
-  variant = 'singleHeading',
-  flags = [],
-  icon,
-}) => {
-  let CardContent;
-  // eslint-disable-next-line default-case
-  switch (variant) {
-    case 'singleHeading':
-      CardContent = <h4 className={`card-content ${textColor}`}>{main}</h4>;
-      break;
-    case 'subHeading':
-      CardContent = (
-        <div className="card-content gap-2">
-          <h2 className={`inline-block tracking-normal ${textColor}`}>
-            {main}
-            <span className="text-gray-400 text-base font-normal">{sub}</span>
-          </h2>
-        </div>
-      );
-      break;
-    case 'error':
-      CardContent = (
-        <h4 className="text-gray-400 card-content">Not Available</h4>
-      );
-      break;
-    case 'flags':
-      CardContent = (
-        <div className="flex gap-1 grow items-center justify-center md:justify-start">
-          {flags.map((flag, index) => (
-            <div
-              className="w-14 aspect-[5/3] relative overflow-hidden rounded"
-              key={index}
-            >
-              <Image
-                src={flag.flag}
-                alt={flag.country}
-                title={flag.country}
-                layout="fill"
-                priority
-              />
-            </div>
-          ))}
-        </div>
-      );
-      break;
-  }
-  return (
-    <div className="card flex flex-col">
-      <h5 className="border-b mb-2 pb-2">
-        {icon && <i className={`fas ${icon}`} />}
-        {` ${title}`}
-      </h5>
-      {CardContent}
-    </div>
+const Content = ({ children }) => (
+  <div className="card-content">{children}</div>
+);
+
+const MainContent = ({ text, textColor = 'text-gray-400', large, children }) =>
+  large ? (
+    <h2 className={`inline-block tracking-normal ${textColor}`}>
+      {text}
+      {children}
+    </h2>
+  ) : (
+    <h5 className={textColor}>
+      {text}
+      {children}
+    </h5>
   );
-};
+
+const SubContent = ({ text }) => (
+  <span className="text-gray-400 text-base font-normal">{text}</span>
+);
+
+Card.Content = Content;
+Card.MainContent = MainContent;
+Card.SubContent = SubContent;
 
 export default Card;
