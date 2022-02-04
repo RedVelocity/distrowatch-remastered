32586ab075b4063dae2be7ac8805e1ee8136a787
Shah Nawaz Khan <blooshaanster@gmail.com>
2022-02-04 11:36:55 +0530

    responsive table without flex/grid patch padding

--- components/layout/distro/DetailsSection.jsx
+++ components/layout/distro/DetailsSection.jsx
@@ -7,11 +7,11 @@ const DetailsSection = ({ details }) => (
             (detail, index) =>
               detail[1].length > 0 && (
                 <tr className="py-1" key={index}>
-                  <td className="block p-2 font-medium md:table-cell md:w-1/4">
+                  <td className="block pt-2 pb-1 font-medium md:p-2 md:table-cell md:w-1/4">
                     {`${detail[0]} `}
                     <i className="w-3 h-3 text-gray-400 align-middle fas fa-external-link-alt" />
                   </td>
-                  <td className="block p-2 md:table-cell">
+                  <td className="block pb-2 md:p-2 md:table-cell">
                     {detail[1].map((links, i) => (
                       <a
                         href={links.link}
