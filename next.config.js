44703c01d2e104220b3e38a0c3b52477e2dc28c2
Shah Nawaz Khan <blooshaanster@gmail.com>
2022-02-07 21:26:00 +0530

    remove @plaiceholder/next

--- next.config.js
+++ next.config.js
@@ -1,21 +1,18 @@
-const { withPlaiceholder } = require('@plaiceholder/next');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withPlaiceholder(
-  withBundleAnalyzer({
-    images: {
-      domains: ['distrowatch.com', 'flagcdn.com'],
-    },
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/distro',
-          permanent: true,
-        },
-      ];
-    },
-  })
-);
+module.exports = withBundleAnalyzer({
+  images: {
+    domains: ['distrowatch.com', 'flagcdn.com'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/distro',
+        permanent: true,
+      },
+    ];
+  },
+});
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,9 @@
 import '../styles/main.css';
 
-const MyApp = ({ Component, pageProps }) => (
+const App = ({ Component, pageProps }) => (
   <div className="page-container">
     <Component {...pageProps} />
   </div>
 );
 
-export default MyApp;
+export default App;
