83847e5555f40bbb0dddd25ba50fe03d0b8e7f90
Shah Nawaz Khan <blooshaanster@gmail.com>
2022-02-09 18:48:40 +0530

    updated eslint for TS

--- src/pages/api/getDistro.ts
+++ src/pages/api/getDistro.ts
@@ -4,7 +4,7 @@ import getDistroDetails from '../../services/getDistroDetails';
 const getDistro = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { distro } = req.query;
-    const pageData = await getDistroDetails(distro);
+    const pageData = await getDistroDetails(distro.toString());
     return res.json(pageData);
   } catch (error) {
     console.log(`error`, error);
