a21167f66c753017220618b350f985cf4348b73b
Shah Nawaz Khan <blooshaanster@gmail.com>
2022-02-16 17:44:32 +0530

    added toggle animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun, faCircle } from '@fortawesome/free-solid-svg-icons';
 import { ColorScheme, SetColorScheme } from '../pages/_app';
 
 const Header = ({
@@ -10,28 +10,35 @@ const Header = ({
 }: {
   colorScheme: ColorScheme;
   setColorScheme: SetColorScheme;
-}): React.ReactElement => (
-  <header className="dark-primary holder flex items-center justify-between">
-    <Image
-      className="rounded-xl shadow dark:grayscale"
-      src="/logo.png"
-      height={48}
-      width={48}
-      priority
-    />
-    <button
-      type="button"
-      onClick={() => {
-        setColorScheme((current) => (current === 'dark' ? 'light' : 'dark'));
-        document.querySelector('html').classList.toggle('dark');
-      }}
-    >
-      <FontAwesomeIcon
-        icon={colorScheme === 'dark' ? faMoon : faSun}
-        className="text-4xl"
+}): React.ReactElement => {
+  const toggleColorScheme = () => {
+    setColorScheme((current) => (current === 'dark' ? 'light' : 'dark'));
+    document.querySelector('html').classList.toggle('dark');
+  };
+  return (
+    <header className="dark-primary holder flex items-center justify-between">
+      <Image
+        className="rounded-xl shadow dark:grayscale"
+        src="/logo.png"
+        height={48}
+        width={48}
+        priority
       />
-    </button>
-  </header>
-);
-
+      <button
+        type="button"
+        className="dark-accent relative flex gap-1 rounded-full p-2 text-xl"
+        onClick={toggleColorScheme}
+      >
+        <FontAwesomeIcon icon={faSun} />
+        <FontAwesomeIcon icon={faMoon} />
+        <FontAwesomeIcon
+          className={`absolute ${
+            colorScheme === 'dark' ? 'translate-x-0' : 'translate-x-6'
+          } transition-transform duration-500 ease-in-out`}
+          icon={faCircle}
+        />
+      </button>
+    </header>
+  );
+};
 export default Header;
