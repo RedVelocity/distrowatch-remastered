e7c141e0ae3c07d4c4c40e67b85edcf8bb042869
Shah Nawaz Khan <blooshaanster@gmail.com>
2022-02-24 21:06:18 +0530

    add framer-motion

--- src/components/layout/distro/HeroSection/index.tsx
+++ src/components/layout/distro/HeroSection/index.tsx
@@ -74,12 +74,9 @@ const HeroSection = ({
             </div>
           </a>
           <motion.h1
-            initial={{ scale: 0.5, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
+            initial={false}
+            animate={{ scale: 1, x: 0, opacity: 1 }}
             transition={{
-              type: 'spring',
-              stiffness: 500,
-              damping: 90,
               duration: 2,
             }}
             layoutId={slug}
