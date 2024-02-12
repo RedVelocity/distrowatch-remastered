b8f907b8777ba610fed6e6e3d30082097b96ba8f
Shah Nawaz Khan <blooshaanster@gmail.com>
2024-02-13 02:10:18 +0530

    update tw.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
-const colors = require('./src/styles/colors');
+import colors from './src/styles/colors';
 
-module.exports = {
-  darkMode: 'class',
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      colors,
-      gridTemplateColumns: {
-        responsive: 'repeat(auto-fit,minmax(10rem,1fr))',
-      },
+export const darkMode = 'class';
+export const content = [
+  './src/pages/**/*.{js,ts,jsx,tsx}',
+  './src/components/**/*.{js,ts,jsx,tsx}',
+];
+export const theme = {
+  extend: {
+    colors,
+    gridTemplateColumns: {
+      responsive: 'repeat(auto-fit,minmax(10rem,1fr))',
     },
   },
-  plugins: [],
 };
+export const plugins = [];
